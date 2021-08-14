e80b0cf5b8789114129a2fb0350325d9927a42e9
Aqib Javeed <aqibjaveed.brandians@gmail.com>
2021-08-14 18:31:31 +0500

    After Drop Table Plugin Deactivate Hook

--- include/database.php
+++ include/database.php
@@ -1,9 +1,8 @@
 <?php
 function custom_plugin_tables(){
 	global $table_prefix, $wpdb;
-	$table_slug = $table_prefix . 'custom_plugin';
-	require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
-	if($wpdb->get_var("SHOW TABLES LIKE '$table_slug'") != $table_slug ){
+	    $table_slug = $table_prefix . 'custom_plugin';
+	    
 		$sql_query_to_create_table = "CREATE TABLE `$table_slug` (";
  		$sql_query_to_create_table .= "`id` int(11) NOT NULL,";
  		$sql_query_to_create_table .= "`name` varchar(150) NOT NULL,";
@@ -14,7 +13,6 @@ function custom_plugin_tables(){
 		// Include Upgrade Script
 		require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
 		dbDelta($sql_query_to_create_table);
-	}
 }
 register_activation_hook(__FILE__, "custom_plugin_tables");
 ?>
--- wp-custom-plugin.php
+++ wp-custom-plugin.php
@@ -18,10 +18,38 @@ define("PLUGIN_VERSION", "1.0");
 
 // PlUGIN INCLUDE
 
-
-include_once PLUGIN_DIR_PATH."/include/database.php";
 include_once PLUGIN_DIR_PATH."/include/custom-css-js.php";
 include_once PLUGIN_DIR_PATH."/include/plugin-menu.php";
 include_once PLUGIN_DIR_PATH."/include/files-view.php";
+
+//PLUGIN DATABASE TABLE CREATE HOOK
+
+function custom_plugin_table_create(){
+		global $table_prefix, $wpdb;
+		$table_slug = $table_prefix . 'custom_plugin';
+		require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+	    if ($wpdb->get_var("SHOW TABLES LIKE '".$table_slug."'") != $table_slug) {
+			$sql_query_to_create_table = "CREATE TABLE `$table_slug` (";
+			$sql_query_to_create_table .= "`id` int(11) NOT NULL AUTO_INCREMENT,";
+			$sql_query_to_create_table .= "`name` varchar(150) NOT NULL,";
+			$sql_query_to_create_table .= "`email` varchar(150) NOT NULL,";
+			$sql_query_to_create_table .= "`phone` varchar(150) NOT NULL,";
+			$sql_query_to_create_table .= "`created_at` timestamp NOT NULL DEFAULT current_timestamp(),";
+			$sql_query_to_create_table .= "PRIMARY KEY (`id`)";
+			$sql_query_to_create_table .= ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+			// INCLUDE UPGRADE SCRIPT
+			dbDelta($sql_query_to_create_table);
+	    }
+}
+register_activation_hook(__FILE__, "custom_plugin_table_create");
+
+//PLUGIN DATABASE TABLE DROP HOOK
+
+function custom_plugin_table_drop(){
+	    global $table_prefix, $wpdb;
+		$table_slug = $table_prefix . 'custom_plugin';
+		$wpdb->query("DROP table IF Exists $table_slug");
+}
+register_deactivation_hook(__FILE__, "custom_plugin_table_drop");
 
 ?>
