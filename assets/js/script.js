9f05742254583f4d8bd65c2711a17dbc84c46c40
Aqib Javeed <aqibjaveed.brandians@gmail.com>
2021-08-29 20:07:40 +0500

    By wp_ajax_{action} Post Data to Server

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
 // Custom Script
 jQuery(function() {
 
-	jQuery("#frmPostOtherPage").on("click", function(e){
-		e.preventDefault();
-		jQuery.post(ajaxurl,{action:"custom_plugin",name:"Aqib", tut:"AJ Creation"}, function(response){
-			console.log(response);
-		});
+	// Other Ajax Request
+	jQuery("#frmPostOtherPage").validate({
+		submitHandler:function() {
+			//console.log("form Pass");
+			var post_data = jQuery("#frmPostOtherPage").serialize()+"&action=custom_ajax_req";
+			jQuery.post(ajaxurl,post_data,function(response) {
+				//console.log(response);
+				var data = jQuery.parseJSON(response);
+				console.log("Name : "+data.txtName+" Email : "+data.txtEmail+" Phone : "+data.txtPhone);
+			});
+		}
 	});
+
+	// jQuery("#frmPostOtherPage").on("click", function(e){
+	// 	e.preventDefault();
+	// 	jQuery.post(ajaxurl,{action:"custom_plugin",name:"Aqib", tut:"AJ Creation"}, function(response){
+	// 		console.log(response);
+	// 	});
+	// });
 
 	jQuery(document).on("click", ".btnClick", function(){
 		
